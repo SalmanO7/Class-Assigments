a2aabfda81f68a7b4d800a5811123c5c9b143e9c
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-11-27 18:25:42 +0500

    11th Class Assigment Complete

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ classWork }: any) {
   return (
     <div className="w-full flex justify-center items-center flex-col">
       <div className="mybio">
@@ -11,14 +11,50 @@ export default function Home() {
           Name : <span className="font-bold">Muhammad Salman</span>
         </h4>
         <h4>
-          Slot : <span className="font-bold" >Sunday 2-5</span>
+          Slot : <span className="font-bold">Sunday 2-5</span>
         </h4>
         <h3>Assigments</h3>
       </div>
-      <div className="bg-green-600 text-white py-3 px-5 rounded-lg  hover:bg-green-800 ">
-        <Link href="/flexbox" className="">
-          FlexBox Assigments
+      <div className="flex  gap-3">
+        <Link
+          href="/flexbox"
+          className="bg-green-600 hover:bg-green-800 text-white rounded-lg py-3 px-5"
+        >
+          FlexBox Assigment
         </Link>
+      </div>
+      <div className="mt-10">
+        <h1 className="text-lg sm:text-2xl md:text-3xl font-bold mx-4">
+          Sunday 11th Class Assigment Complete
+        </h1>
+        <div className="flex flex-col justify-center items-start pl-16">
+          <div>
+            Name:{" "}
+            <span className="font-bold text-green-500">
+              {classWork.Student}
+            </span>
+          </div>
+          <div>
+            Teacher:{" "}
+            <span className="font-bold text-green-500">
+              {classWork.Teacher}
+            </span>
+          </div>
+          <div>
+            Task 1:{" "}
+            <span className="font-bold text-green-500"> {classWork.Task1}</span>
+          </div>
+          <div>
+            Task 2:{" "}
+            <span className="font-bold text-green-500">{classWork.Task2}</span>
+          </div>
+          <div>
+            Task:{" "}
+            <span className="font-bold text-green-500">
+              {classWork.Complete}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import Home from "./components/Home";
 
 const page = () => {
+  const classWork: {
+    Student: string;
+    Teacher: string;
+    Task1: string;
+    Task2: string;
+    Complete: string;
+  } = {
+    Student: "M Salman",
+    Teacher: "Sir Ali Jawwad",
+    Task1: "Pass props to Child Component",
+    Task2: "Render props Child Component",
+    Complete: "Task Completed",
+  };
+
   return (
     <div>
-      <Home />
+      <Home classWork={classWork} />
     </div>
   );
 };
